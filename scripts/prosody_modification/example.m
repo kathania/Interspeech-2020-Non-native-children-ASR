5861984b94a83b0d556ac40b9ff97e180b037d2f
Hemant Kathania <hemant.sbcet@gmail.com>
2020-08-23 22:40:07 +0300

    Update example.m

--- scripts/prosody_modification/example.m
+++ scripts/prosody_modification/example.m
@@ -4,6 +4,7 @@ close all;
 
 
 
+
 FID1=fopen('list of wav file name');
     
 i=1;
@@ -11,8 +12,10 @@ while ~feof(FID1)
     
     
    line1=fgetl(FID1);
-
+%%%%%%%%%%%%%%%%%  pitch modification %%%%%%%%%%%%%%%%%%%%%%
 pitch_modification(line1, -2);
+
+%%%%%%%%%%%%%%%%%  speaking-rate modification  %%%%%%%%%%%%%%%%%%%%%%
 speaking-rate_using_stftm(line1, 0.8);
 
 i=i+1
